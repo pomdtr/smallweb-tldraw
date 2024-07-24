5d327b2d8fb322f403cd5255f67348714f3cf3d4
pomdtr <achille.lacoin@gmail.com>
2024-07-24 19:20:08 +0200

    automatically save drawing

--- dist/main.ts
+++ dist/main.ts
@@ -3,8 +3,27 @@ import { serveStatic } from "@hono/hono/deno";
 
 const app = new Hono();
 
-app.get("/api/hello", (c) => {
-    return c.json({ message: "Hello, World!" });
+app.get("/api/load", () => {
+    const snapshot = localStorage.getItem("snapshot");
+    if (!snapshot) {
+        return new Response(null, {
+            status: 204,
+        });
+    }
+
+    return new Response(snapshot, {
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+});
+
+app.post("/api/save", async (c) => {
+    const drawing = await c.req.text();
+    localStorage.setItem("snapshot", drawing);
+    return new Response(null, {
+        status: 204,
+    });
 });
 
 app.get(
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,90 @@
-import { Tldraw } from 'tldraw'
-import './index.css'
+import {
+	createTLStore,
+	debounce,
+	getSnapshot,
+	loadSnapshot,
+	Tldraw,
+	type TLStoreWithStatus,
+} from "tldraw";
+import { useEffect, useState } from "react";
+import "./index.css";
+
+async function getRemoteSnapshot() {
+	const response = await fetch("/api/load");
+	if (response.status === 204) {
+		return null;
+	}
+	return response.json();
+}
+
+async function saveRemoteSnapshot(snapshot: any) {
+	await fetch("/api/save", {
+		method: "POST",
+		body: JSON.stringify(snapshot),
+	});
+}
 
 export default function App() {
+	const [storeWithStatus, setStoreWithStatus] = useState<TLStoreWithStatus>({
+		status: "loading",
+	});
+
+	useEffect(() => {
+		let cancelled = false;
+		async function loadRemoteSnapshot() {
+			// Get the snapshot
+			const snapshot = await getRemoteSnapshot();
+			if (cancelled) return;
+			if (!snapshot) {
+				// No snapshot found
+				const newStore = createTLStore();
+				setStoreWithStatus({
+					connectionStatus: "online",
+					store: newStore,
+					status: "synced-remote",
+				});
+				return;
+			}
+
+			// Create the store
+			const newStore = createTLStore();
+
+			// Load the snapshot
+			loadSnapshot(newStore, snapshot);
+
+			// Update the store with status
+			setStoreWithStatus({
+				connectionStatus: "online",
+				store: newStore,
+				status: "synced-remote",
+			});
+		}
+
+		loadRemoteSnapshot();
+
+		return () => {
+			cancelled = true;
+		};
+	}, []);
+
 	return (
-		<div style={{ position: 'fixed', inset: 0 }}>
-			<Tldraw />
+		<div style={{ position: "fixed", inset: 0 }}>
+			<Tldraw
+				store={storeWithStatus}
+				onMount={(editor) => {
+					editor.store.listen(
+						debounce(async () => {
+							console.log("Store changed");
+							const snapshot = getSnapshot(editor.store);
+							await saveRemoteSnapshot(snapshot);
+						}, 1000),
+						{
+							scope: "document",
+							source: "user",
+						},
+					);
+				}}
+			/>
 		</div>
-	)
+	);
 }
